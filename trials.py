95038f70ac9b813727ef41c985c57df27d96cd83
Heather1337 <heatherlynn1337@gmail.com>
2020-10-20 14:00:23 -0700

    Solving Python functions.

--- trials.py
+++ trials.py
@@ -2,44 +2,123 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    evenNums = []
+
+    for num in nums:
+        if num % 2 == 0:
+            evenNums.append(num)
+
+    return evenNums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+
+    result = []
+
+    for i in range(len(items)):
+        if i % 2 != 0:
+            result.append(items[i])
+
+    return result
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    i = 1
+
+    for item in items:
+        print(f"{i}. {item}")
+
+        i += 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    
+    nums = []
+
+    for num in range(start, stop):
+        nums.append(num)
+
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    
+    chars = []
 
+    for letter in word:
+        if letter in 'aeiou':
+            chars.append('*')
+        chars.append(letter)
+
+    return ''.join(chars)
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    
+    camel_case = []
+
+    for word in string.split('_'):
+        camel_case.append(f'{word[0].upper()} {word[1:]}')
+
+    return ''.join(camel_case)
 
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    
+    longest = len(words[0])
+
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+
+    return longest
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+
+    result = []
+
+    for char in string:
+        if len(result) == 0 or char != result[len(result) - 1]:
+            result.append(char)
+
+    return ''.join(result)
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    parens = 0
+
+    for char in string:
+        if char == '(':
+            parens += 1
+        elif char == ')':
+            parens -= 1
+            if parens < 0:
+                return False
+
+    return parens < 0
 
 
 def compress(string):
-    pass  # TODO: replace this line with your code
+
+    compressed = []
+    curr_char = ''
+    char_count = 0
+
+    for char in string:
+        if char != curr_char:
+            compressed.append(curr_char)
+            if char_count > 1:
+                compressed.append(str(char_count))
+            curr_char = char
+            char_count = 0
+        char_count += 1
+
+    compressed.append(curr_char)
+    if char_count > 1:
+        compressed.append(str(char_count))
+    return ''.join(compressed)
